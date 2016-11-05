6f276f8ad05dd715d07e44725b173a7708337b06
manzam <zambacevicius.mantas@gmail.com>
2016-11-05 18:42:06 +0200

    UTC set to europe/vilnius.

--- web/app.php
+++ web/app.php
@@ -8,6 +8,7 @@ include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
+date_default_timezone_set('Europe/Vilnius');
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
